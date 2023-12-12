7319719b96241e013146a99033b23a268b02c102
EL-CLEAR <jph0102@brich.co.kr>
2023-12-13 00:34:58 +0900

    feat : 기본 request 추가

--- app/Http/Requests/MySolutionRequest.php
+++ app/Http/Requests/MySolutionRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
+
 class MySolutionRequest extends FormRequest
 {
     /**
@@ -25,7 +26,7 @@ class MySolutionRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+
         ];
     }
 }
