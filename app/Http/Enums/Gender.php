a7d04b4ffabbde9f01a3e1c32f263fbab1ea3722
EL-CLEAR <jph0102@brich.co.kr>
2023-12-13 00:16:23 +0900

    feat : enum class 추가

---
+++ app/Http/Enums/Gender.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Enums;
+
+enum Gender: string
+{
+    case MAN = "man";
+    case WOMAN = "woman";
+}
---
+++ app/Http/Enums/LifestyleTag.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Enums;
+
+enum LifestyleTag: string
+{
+    case ENOUGH_MONET = "enough_money";
+    case STRONG_WILL = "strong_will";
+    case ENOUGH_TIME = "enough_time";
+}
