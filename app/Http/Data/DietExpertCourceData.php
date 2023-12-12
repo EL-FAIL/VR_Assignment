d4ddf83d47ee821f1137dbc8429f286e702f8b92
EL-CLEAR <jph0102@brich.co.kr>
2023-12-13 02:12:28 +0900

    feat : 검색을 위한 data class 추가

---
+++ app/Http/Data/DietExpertCourceData.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Data;
+
+use App\Http\Enums\RecommendSolution;
+
+class DietExpertCourceData
+{
+    const array DIET_EXPERT_COURSE = [
+        "Intermittent Fasting" => [
+            "enough_time",
+            "strong_will"
+        ],
+        "LCHF" => [
+            "enough_money"
+        ]
+    ];
+
+    public static function getCourse(): array
+    {
+        return self::DIET_EXPERT_COURSE;
+    }
+}
